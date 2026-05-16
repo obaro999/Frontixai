ad40e691995d47f4b400c2179389de08d075cb2c
Iteire Obaro <frontixai@gmail.com>
2026-05-16 01:00:59 +0100

    fix main urls.py

--- frontix_project/urls.py
+++ frontix_project/urls.py
@@ -1,13 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.login_view, name='login'),
-    path('register/', views.register_view, name='register'),
-    path('logout/', views.logout_view, name='logout'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('book/<int:business_id>/', views.book_appointment, name='book_appointment'),
-    path('payment/<int:business_id>/', views.payment_info, name='payment_info'),
-    path('cancel/<int:appointment_id>/', views.cancel_appointment, name='cancel_appointment'),
-    path('settings/payment/', views.update_payment_info, name='update_payment_info'),
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
 ]
